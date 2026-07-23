4f542809541997264cdb73043174d8c92f1ddccf
Treasure Tamb <treasuretamb@gmail.com>
2026-07-23 07:40:07 +0200

    Week Problem solving: 1 - 3

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,24 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        // TODO Problem 1
+        var results = new List<string>();
+        var seen = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            var reversed = new string(new[] { word[1], word[0] });
+
+            if (seen.Contains(reversed))
+            {
+                results.Add($"{word} & {reversed}");
+            }
+            else
+            {
+                seen.Add(word);
+            }
+        }
+        return results.ToArray();
     }
 
     /// <summary>
@@ -42,7 +58,17 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            // TODO Problem 2 
+            var degree = fields[3];
+
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
@@ -66,8 +92,36 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        // TODO Problem 3
+        var letterCounts = new Dictionary<char, int>();
+
+        foreach (var letter in word1.ToLower())
+        {
+            if (letter == ' ') continue;
+
+            if (letterCounts.ContainsKey(letter))
+                letterCounts[letter] += 1;
+            else
+                letterCounts[letter] = 1;
+        }
+
+        foreach (var letter in word2.ToLower())
+        {
+            if (letter == ' ') continue;
+
+            if (letterCounts.ContainsKey(letter))
+                letterCounts[letter] -= 1;
+            else
+                letterCounts[letter] = -1;
+        }
+
+        foreach (var count in letterCounts.Values)
+        {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
     }
 
     /// <summary>
